3c4e830bd788f48d2e27ef2a3dd8b31bbf81ac9c
hparten <hallieparten@gmail.com>
2023-03-28 19:24:12 -0500

    package

---
+++ demo/simple.py
@@ -0,0 +1,4 @@
+def printer():
+    ''' this function takes in no input and outputs a message'''
+    print("this is my super simple function")
+
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Demo',
+      version='0.1',
+      description='super simple module',
+      author='hallie parten',
+      author_email='hallie parten',
+      license='APACHE',
+      packages=['demo'])
